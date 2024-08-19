646f970cfab8e956097a9faccc3981f5cac59796
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2024-08-20 01:01:10 +0530

    just made both the mini projects looking forward to make changes to these projects such as advance error handling and UI. Programming is FUN!!!

---
+++ FizzBuzz.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MiniProjects
+{
+    public class FizzBuzzGame()
+    {
+        public static void BuzzFizz()
+        {
+            Console.Write("Type the last number of the range staring from one: ");
+            string num = Console.ReadLine();
+            int rangeNum;
+            bool success = int.TryParse(num, out rangeNum);
+            if (success) 
+            {
+                for (int i = 1; i <= rangeNum; i++)
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        Console.WriteLine("Great! This is a FIZZBUZZ!!! {0} " +
+                            "is divisible by both the numbers 3 & 5", i);
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Console.WriteLine("This is a FIZZ! {0} is divisible by 3", i);
+                    }
+                    else if (i % 5 == 0)
+                    {
+
+                        Console.WriteLine("This is a BUZZ! {0} is divisible by 5", i);
+                    }
+                    else
+                    {
+                        Console.WriteLine("This number: {0} is not divisible by both the numbers", i);
+                    }
+                    
+                }
+            }
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 
-namespace TimesTable
+namespace MiniProjects
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            TimesTable.Table.TimesTableCalculation();
+            do
+            {
+                Console.WriteLine("Which MiniProject you want to run\n");
+                Console.WriteLine("1.Tables Calculation");
+                Console.WriteLine("2.Fizz Buzz Game");
+                Console.WriteLine("3.Exit");
+
+                Console.Write("Type Your Choice: ");
+                string num = Console.ReadLine();
+                int choice;
+                bool success = int.TryParse(num, out choice);
+
+                if (success)
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            MiniProjects.Table.TimesTableCalculation();
+                            break;
+                        case 2:
+                            MiniProjects.FizzBuzzGame.BuzzFizz();
+                            break;
+                        case 3:
+                            Console.WriteLine("Thank you!!!");
+                            return;
+                            break;
+                        default:
+                            Console.WriteLine("Wrong Input. OverFlow Exception");
+                            break;
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            } while (true);
+            
+
+            
+            
         }
     }
    
