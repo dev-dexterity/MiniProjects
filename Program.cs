9f2f8f69c7392b9de3cfe94382b92579043f73d4
Omkar Gaikwad <80238904+omkarcodes@users.noreply.github.com>
2024-08-20 00:21:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TimesTable
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TimesTable.Table.TimesTableCalculation();
+        }
+    }
+   
+}
---
+++ TimesTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TimesTable
+{
+    public class Table
+    {
+        public static void TimesTableCalculation()
+        {
+            Console.Write("Type the number you want the table of: ");
+            string value = Console.ReadLine();
+            int tableNum;
+            bool success = int.TryParse(value, out tableNum);
+
+            if (success)
+            {
+                Console.WriteLine("Successful, {0} ", tableNum);
+            }
+            else
+            {
+                Console.WriteLine("Error Conversion Exception");
+            }
+
+            
+
+            for(int i = 1;i <= 10; i++)
+            {
+                int result = tableNum * i;
+                Console.WriteLine("{0} * {1} = {2} ", tableNum, i, result);
+            }
+        }
+    }
+}
